091f69e00be0d36ddca3afa9d5059f4075d06ba2
Cedric Williams <me@cedricwilliams.com>
2014-07-27 16:42:26 -0400

    Cedric's Programming Assignment 2
    Prog. 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,67 @@
 ## functions do
 
 ## Write a short comment describing this function
+##input matrix as x
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  #store matrix into cache
+  m_1 <<- as.matrix(x)
+  
+  #creates inverse matrix variable thats empty
+  m_i <<- NULL
+ 
+  #creates the set function for changing the original matrix
+  set <- function(y) {
+  
+  #stores matrix into cache
+  m_1 <<- as.matrix(y)
+    
+  }
+  
+  #gets non-inverse matrix
+  get_matrix <- function() m_1
+  
+  #sets inverse matrix
+  set_inverse <<- function(solve_inverse) m_i <<- solve_inverse
+  
+  #gets inverse matrix
+  get_inverse <<- function() m_i
+  
+  #list all the functions created and passed
+  list(set=set, get_matrix=get_matrix, set_inverse=set_inverse, get_inverse=get_inverse)
+  
 }
 
 
 ## Write a short comment describing this function
+##computes, inverses and stores matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        
+  
+  
+
+  if(is.null(m_i)){
+    
+  
+  #new_data <- (x)
+ 
+  ## Inverses original matrix and stores into non-cached variable
+  m_i <- solve(m_1, ...)
+  
+  ## Runs the set_inverse function
+  set_inverse(m_i)
+  
+  ## Return a matrix that is the inverse of 'x'
+  return(m_i)
+  
+  }
+  
+  message("Yo...Pulling it Down. For Shizzle!")
+  
+  return(m_i) 
+
+  }
+  
+  
